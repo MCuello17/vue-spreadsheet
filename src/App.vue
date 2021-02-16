0396e86d905072fc44e738e3f408dba415179c30
Michael Cuello <mjcuello17@gmail.com>
2021-02-16 16:05:43 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <spreadsheet/>
+  </div>
+</template>
+
+<script>
+import Spreadsheet from './components/Spreadsheet'
+
+export default {
+  name: 'App',
+  components: {
+    Spreadsheet
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Spreadsheet.vue
@@ -0,0 +1,10 @@
+<template>
+    <table style="width:100%">
+    </table>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+</style>
