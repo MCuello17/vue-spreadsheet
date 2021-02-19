5e43bb4a5a848f6590056248838534115380c85e
Michael Cuello <mjcuello17@gmail.com>
2021-02-19 19:27:59 -0300

    Css variables

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div
+    id="app">
     <spreadsheet/>
   </div>
 </template>
@@ -16,26 +17,56 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0;
-}
-* {
-  box-sizing: border-box;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  overflow: scroll;
-  width: 100vw;
-  height: 100vh;
-  padding: 10px;
-}
-@media (min-width: 768px) {
-  #app {
-    overflow: hidden;
+  :root {
+    /* Colors */
+    --background: #fff;
+    --shadow-color: #00000027;
+    --primary: #439aff;
+    --separator-highlight: #bbbbbb;
+    --separator: #f3f3f3;
+    --button: var(--separator);
+    --error: #ff5858;
+    --text: #000225;
+    /* Misc */
+    --shadow: 0px 0px 15px 2px var(--shadow-color);
   }
-}
+
+  body {
+    margin: 0;
+    background: transparent;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  #app.darkmode {
+    --background: #292835;
+    --shadow-color: #ffffff27;
+    --primary: #3cb823;
+    --separator: #444349;
+    --separator-highlight: #494a69;
+    --button: #151625;
+    --error: #ff5858;
+    --text: #d1d1d1;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    background: var(--background);
+    color: var(--text);
+    overflow: scroll;
+    width: 100vw;
+    height: 100vh;
+    padding: 10px;
+  }
+
+  @media (min-width: 768px) {
+    #app {
+      overflow: hidden;
+    }
+  }
 </style>
