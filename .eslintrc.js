c01766f0f03c29d268e6084c60b58c0f70011866
Michael Cuello <mjcuello17@gmail.com>
2021-02-19 10:54:05 -0300

    Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,34 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-indent': ['error', 2, {
+      attribute: 1,
+      baseIndent: 1,
+      closeBracket: 0,
+      alignAttributesVertically: true,
+      ignores: []
+    }],
+    "vue/html-self-closing": ["error", {
+      html: {
+        void: "never",
+        normal: "always",
+        component: "always"
+      },
+      svg: "always",
+      math: "always"
+    }],
+    "vue/multiline-html-element-content-newline": ["error", {
+      ignoreWhenEmpty: true,
+      ignores: ["pre", "textarea"],
+      allowEmptyLines: false
+  }],
+  "vue/max-attributes-per-line": ["error", {
+    singleline: 1,
+    multiline: {
+      max: 1,
+      allowFirstLine: false
+    }
+  }]
   }
 }
